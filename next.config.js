0ef7aed8712926d28c2504cf919a82ab6f1ef4a3
Manuel Firman <manuel.firman@gmail.com>
2024-10-04 22:44:50 -0300

    fix: test img url

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: process.env.NEXT_PUBLIC_CLOUDFRONT_URL || '/',
     images: {
-      domains: [process.env.NEXT_PUBLIC_CLOUDFRONT_URL],
       unoptimized: true,
     },
 }
--- src/components/BackgroundRadialLeft/BackgroundRadialLeft.tsx
+++ src/components/BackgroundRadialLeft/BackgroundRadialLeft.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 export function BackgroundRadialLeft() {
+    const imgUrl = `${process.env.NEXT_PUBLIC_CLOUDFRONT_URL}/assets/background-floated-left.png`
     return (
         <div className="left-0 absolute -z-50 top-0">
-            <Image src="/assets/background-floated-left.png" width={850} height={650} alt="Circle Left" />
+            <Image src={imgUrl} width={850} height={650} alt="Circle Left" />
         </div>
     )
 }
