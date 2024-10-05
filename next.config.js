877ba3c37dfc52d0592fcf6d73b8579b49baf9a9
Manuel Firman <manuel.firman@gmail.com>
2024-10-04 22:03:08 -0300

    fix: assetPrefix changed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: '/',
+    assetPrefix: process.env.NEXT_PUBLIC_CLOUDFRONT_URL || '/',
     images: {
       unoptimized: true,
     },
