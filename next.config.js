f6379b7e07734dc905d36ccab1adcd67d5d96e7a
Manuel Firman <manuel.firman@gmail.com>
2024-10-04 22:33:07 -0300

    fix: trying to set url img | next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    assetPrefix: process.env.NEXT_PUBLIC_CLOUDFRONT_URL || '/',
     images: {
+      domains: [process.env.NEXT_PUBLIC_CLOUDFRONT_URL],
       unoptimized: true,
     },
 }
